093afc6efd743d03c755d2687b943a28548ef9c1
explosivetomatoe <jorgejedieduardo@gmail.com>
2016-04-18 18:19:06 -0600

    Update Polinomio.cpp

--- CppApplication_1/Polinomio.cpp
+++ CppApplication_1/Polinomio.cpp
@@ -44,6 +44,21 @@ double Polinomio::eval(double x, double y){
 }
 
 string Polinomio::aHil(){
+    //ejemplo de commo separar en un char especifico y hacer un array
+    /*
+    string str = "102:330:3133:76531:451:000:12:44412";
+for (int i=0; i<str.length(); i++)
+{
+    if (str[i] == ':')
+        str[i] = ' ';
+}
+
+vector<int> array;
+stringstream ss(str);
+int temp;
+while (ss >> temp)
+    array.push_back(temp); // done! now array={102,330,3133,76531,451,000,12,44412}
+    */
     
 }
 
