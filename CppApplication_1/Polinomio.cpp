721080d3128967e6ae95fd29e6bf7c3c4031ee1a
stvcastro9 <stvcastro9@gmail.com>
2016-04-28 18:41:19 -0600

    Update Polinomio.cpp

--- CppApplication_1/Polinomio.cpp
+++ CppApplication_1/Polinomio.cpp
@@ -23,6 +23,22 @@ Polinomio::Polinomio(const Polinomio& orig) {
 
 Polinomio::Polinomio(const string & expoli) {
 
+     //ejemplo de commo separar en un char especifico y hacer un array
+    /*
+    string str = "102:330:3133:76531:451:000:12:44412";
+    for (int i=0; i<str.length(); i++)
+    {
+        if (str[i] == ':')
+        str[i] = ' ';
+    }
+
+    vector<int> array;
+    stringstream ss(str);
+    int temp;
+    while (ss >> temp)
+    array.push_back(temp); // done! now array={102,330,3133,76531,451,000,12,44412}
+     *
+   */
 }
 
 Polinomio::~Polinomio() {
@@ -53,33 +69,16 @@ double Polinomio::eval(double x, double y) {
 }
 
 string Polinomio::aHil() {
-    //ejemplo de commo separar en un char especifico y hacer un array
-    /*
-    string str = "102:330:3133:76531:451:000:12:44412";
-    for (int i=0; i<str.length(); i++)
-    {
-        if (str[i] == ':')
-        str[i] = ' ';
-    }
-
-    vector<int> array;
-    stringstream ss(str);
-    int temp;
-    while (ss >> temp)
-    array.push_back(temp); // done! now array={102,330,3133,76531,451,000,12,44412}
-     *
-   */
     stringstream fs;
     shared_ptr<NdoPoli> p = inicio;
     
     while (p != nullptr) {
         fs << p ->expoli;
-        p = p->sgt;       
+        p = p->sgt;   
+        if (p->sgt != nullptr)
+            fs << '+';
     }
     return fs.str();
-
-    
-    
 }
 
 bool Polinomio::verInv() {
